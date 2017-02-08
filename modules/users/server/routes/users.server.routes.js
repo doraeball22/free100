4bc71ef794119bcfd620115458fc4b74a07c09d4
doraeball22 <anurak_b@cmu.ac.th>
2017-02-08 16:31:30 +0700

    add bank account in User schema

---
+++ modules/users/server/routes/users.server.routes.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function (app) {
+  // User Routes
+  var users = require('../controllers/users.server.controller');
+
+  // Setting up the users profile api
+  app.route('/api/users/me').get(users.me);
+  app.route('/api/users').put(users.update);
+  app.route('/api/users/accounts').delete(users.removeOAuthProvider);
+  app.route('/api/users/password').post(users.changePassword);
+  app.route('/api/users/picture').post(users.changeProfilePicture);
+  app.route('/api/users/bank').post(users.updateBankAccount);
+
+  //  render account bank form
+  app.route('/about').get(users.about);
+
+  // Finish by binding the user middleware
+  app.param('userId', users.userByID);
+};
